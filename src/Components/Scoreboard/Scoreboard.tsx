af608359a711010ecf558c5e4941f2b99f1e4eee
ritabrilhante <rita.brilhante@basement.io>
2021-10-25 14:28:01 -0300

    create Scoreboard component

--- src/Components/Scoreboard/Scoreboard.tsx
+++ src/Components/Scoreboard/Scoreboard.tsx
@@ -3,6 +3,15 @@ import './Scoreboard.css'
 function Scoreboard() {
     return (
       <section className="scoreboard">
+        <article id="xis-player">
+          <img src="img/xis.svg"/>
+          <p id="result-xis">1</p>
+        </article>
+
+        <article id="ball-player">
+          <img src="img/ball.svg"/>
+          <p id="result-ball">1</p>
+        </article>
       </section>
     );
   }
