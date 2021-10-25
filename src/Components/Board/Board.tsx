e4a6fcfd335d3a43618a158db4e058f3123f2276
ritabrilhante <rita.brilhante@basement.io>
2021-10-25 13:59:57 -0300

    set up environment, init repository

---
+++ src/Components/Board/Board.tsx
@@ -0,0 +1,11 @@
+import './Board.css'
+
+function Board() {
+    return (
+      <div className="App">
+          Teste;
+      </div>
+    );
+  }
+
+export default Board;
---
+++ src/Components/Scoreboard/Scoreboard.tsx
@@ -0,0 +1,11 @@
+import './Scoreboard.css'
+
+function Scoreboard() {
+    return (
+      <div className="App">
+          Teste;
+      </div>
+    );
+  }
+
+export default Scoreboard;
